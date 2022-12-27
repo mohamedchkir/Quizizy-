8a61a73180fae1d560962cdc1140c7ac47b50c7e
mohamed chkir <mohamedchkir2@gmail.com>
2022-12-28 00:48:52 +0100

    travailler sur le style est l'affichage des qst et les div

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,4 @@
 let DATA;
-
 fetch("http://localhost:3000/quiz/1")
   .then((data) => data.json())
   .then((data) => {
@@ -13,7 +12,7 @@ const quiz = document.querySelector('.quiz-app');
 const next_btn = document.getElementById("next")
 const miniteur = document.querySelector(".seconds")
 const questi = document.querySelector(".count")
-
+const lets = document.getElementById("lets")
 // console.log(miniteur)
 
 // function timer() {
@@ -28,35 +27,24 @@ const questi = document.querySelector(".count")
 //     }
 // }
 
-// submit
+//! submit
 next_btn.addEventListener("click", function (e) {
   check()
   clearInterval(interval);
   afficher_question(DATA.questions[index]);
 
-
-  // e.preventDefault();
-  // time = 0;
-  // clearInterval(interval)
-  // interval = setInterval(timer, 1000);
 })
 
+//todo timer 
 function timer() {
   let seconds = 5;
-
-  // timer 
   if (seconds > 0) {
     interval = setInterval(() => {
       console.log(seconds);
       miniteur.innerText = "You have" + " " + seconds;
       seconds--;
-
-
-
       if (seconds < 0) {
         clearInterval(interval);
-
-
         afficher_question(DATA.questions[index]);
       }
     }, 1000);
@@ -65,41 +53,14 @@ function timer() {
   }
 }
 
-
-
-
-
-function addtohtml(data) {
-  console.log(data)
-
-  // data.forEach(ele => {
-
-  //     ele.questions.forEach((qst) => {
-  //         let temp = `
-  //         <h3>${qst.content}</h3>
-  //         <p>${qst.options[0].content}</p>
-  //         <p>${qst.options[1].content}</p>
-  //         <p>${qst.options[2].content}</p>
-  //         <p>${qst.options[3].content}</p>
-  //         `
-  //         output += temp
-  //     })
-
-  // });
-  // let index = 0;
-
-}
-
-
-
-
 function afficher_question(question) {
 
   console.log(index);
-  // check index == 10 
+
   if (index >= 10) {
     console.log(list);
     clearInterval(interval);
+    endQuiz();
     return;
   } else {
     document.querySelector('#quest-num').textContent = (index + 1) + ' / 10';
@@ -129,50 +90,25 @@ function afficher_question(question) {
               <label for="answer-4">${question.options[3].content}</label>
             </div>
           </div>
-    
           
           `;
-
   document.querySelector("#question").innerHTML = output;
   index++;
 }
 
-
-document.querySelector('#lets').onclick = () => {
-  console.log('gg');
-  window.location = "index1.html";
-}
-
-
-function starQuiz(e) {
-  e.preventDefault();
-
-  let username = document.getElementById("username").value;
-  document.getElementById("username").innerHTML = `Hello ${username}`;
-
-  if (username = "") {
-    document.getElementById("empty-input").innerHTML = "Please Enter your Username";
-
-  }
-}
-
-
-
-// a condition switch div class namein none 
+// a condition switch div class name in none 
 const r = document.querySelector(':root');
 const BTN_PLAY = document.querySelector('#btn-play');
-const letgo = document.querySelector(".lets")
-
 
 BTN_PLAY.onclick = () => {
 
   r.style.setProperty('--width-stepper', '50%');
   document.querySelector('.step-2').classList.add('active');
-  quiz.querySelector('#guide').classList.add('none');
+  quiz.querySelector('.questions-guide').classList.add('none');
   quiz.querySelector('.quiz-questions').classList.remove('none');
-
   clearInterval(interval);
   afficher_question(DATA.questions[index]);
+
 }
 
 
@@ -181,24 +117,57 @@ BTN_PLAY.onclick = () => {
 
 let list = []
 function check() {
-  console.log(index + 'fffff');
-  let val
+
+  let val = null;
   let checks = document.getElementsByName('questions')
 
-
+  let obj = {
+    "id-quest": DATA.questions[index - 1].id,
+    "id-checked": 999
+  };
   for (let i = 0; i < checks.length; i++) {
-
     if (checks[i].checked) {
-
-
-      list.push({
-        "id-quest": DATA.questions[index - 1].id,
-        "id-checked": i + 1
-      })
-
+      obj["id-checked"] = i + 1;
     }
+  }
+  list.push(obj)
+  console.log(list);
+}
+
+function checkAnswers() {
+  for (let i = 0; i < list.length; i++) {
+    console.log(list[i]);
 
   }
-  console.log(list);
+}
+
+// * LA FIN DE QUIZ
+function endQuiz() {
+  check();
+  r.style.setProperty('--width-stepper', '100%');
+  document.querySelector('.step-3').classList.add('active');
+  quiz.querySelector('.quiz-questions').classList.add('none');
+  document.querySelector('.result').classList.remove('none');
+
 
 }
+
+
+// lets.addEventListener("click", function (e) {
+//   let username = document.getElementById("username").value
+//   alert(username)
+//   sessionStorage.setItem("user", document.getElementById("username").value);
+//   let name = sessionStorage.getItem("user");
+
+// })
+
+// ! USERNAME 
+function getname() {
+  sessionStorage.setItem("user", document.getElementById("username").value);
+}
+let name = sessionStorage.getItem("user");
+document.getElementById("username").innerText = `Hello ` + name;
+
+
+
+
